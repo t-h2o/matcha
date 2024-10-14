50406419156fbaae46aa684fd5c6fc95ee42974e
Robin Burri <robinburri@tutanota.com>
2024-10-14 17:07:26 +0200

    add links to the navbar

--- frontend/AngularApp/src/app/app.component.ts
+++ frontend/AngularApp/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponentSimple } from './login/navbarSimple/navbarsimple.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponentSimple],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
