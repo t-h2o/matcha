8da55cf0e087dd0959d81926237e834a87fd95ee
Robin Burri <robinburri@tutanota.com>
2024-10-14 19:59:13 +0200

    Frontend routing (#3)
    * clean app component
    
    * add 4 routes

--- frontend/AngularApp/src/app/app.routes.ts
+++ frontend/AngularApp/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { BrowsingComponent } from './browsing/browsing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'browsing',
+    component: BrowsingComponent,
+  },
+];
