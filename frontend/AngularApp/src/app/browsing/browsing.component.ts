c249015a2c6749c2cc1472a0abd79f362b8f336d
Robin Burri <robinburri@tutanota.com>
2024-10-14 16:14:26 +0200

    add 4 routes

---
+++ frontend/AngularApp/src/app/browsing/browsing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-browsing',
+  standalone: true,
+  imports: [],
+  templateUrl: './browsing.component.html',
+  styleUrl: './browsing.component.scss'
+})
+export class BrowsingComponent {
+
+}
