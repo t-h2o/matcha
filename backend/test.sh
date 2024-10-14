af1773e0e38bf8dec7f74216a154595f88ca9ced
|°_°| <grivel.theo@protonmail.com>
2024-10-14 17:45:25 +0200

    test: WIP: function

--- backend/test.sh
+++ backend/test.sh
@@ -1,6 +1,42 @@
-curl localhost:5001
+URL="localhost:5001"
 
-curl localhost:5001/create
+compare() {
+		CMD="curl --silent http://${URL}${1}"
+		ANSWER="$(${CMD})"
+        if [ "${ANSWER}" != "${2}" ] ; then
+                echo "error with \"$1\""
+				echo "executed: ${CMD}"
+				echo "received: ${ANSWER}"
+                exit 1
+        fi
+        echo "success: \"${1}\""
+}
+
+compare "" "<h1>Hello, World!</h1>"
+
+compare "/create" "created"
+
+#compare \
+#	"/register --data \"username=user\" --data \"password=1234\"" \
+#   	"User user was succefull added"
+
+compare \
+	"/register --data \"username=user\" --data \"password=1234\"" \
+    "error: User user is already registered."
+
+exit 0
+#created
+#error: Username is required.
+#error: Password is required.<!doctype html>
+
+
+curl localhost:5001/register \
+  --data "username=user" \
+  --data "password=1234"
+
+curl localhost:5001/register \
+  --data "username=user2" \
+  --data "password=1234"
 
 curl localhost:5001/register \
   --data "username=user" \
