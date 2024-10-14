c8aa3dd91c610a9bbb641f923ec31b2d42da6456
Robin Burri <robinburri@tutanota.com>
2024-10-14 20:10:53 +0200

    change login-form into login

--- frontend/AngularApp/src/app/app.component.ts
+++ frontend/AngularApp/src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { CardComponent } from './UI/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LoginFormComponent, CardComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-app';
--- frontend/AngularApp/src/app/login/login.component.ts
+++ frontend/AngularApp/src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../UI/card/card.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [LoginFormComponent, CardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
