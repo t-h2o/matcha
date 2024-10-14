bcbe5cc4df692716f5a9b479d1954020bbe02581
Robin Burri <robinburri@tutanota.com>
2024-10-14 20:03:36 +0200

    fix conflicts

---
+++ frontend/AngularApp/src/app/navbarSimple/navbarsimple.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarsimple',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbarsimple.component.html',
+  styleUrl: './navbarsimple.component.scss'
+})
+export class NavbarComponentSimple {
+
+}
