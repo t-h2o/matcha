8f5b6c362d109e4266d42ac1be80287ca3ac9b00
Robin Burri <robinburri@tutanota.com>
2024-10-14 20:00:28 +0200

    login form created

--- frontend/AngularApp/src/app/app.component.ts
+++ frontend/AngularApp/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { CardComponent } from './UI/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginFormComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
