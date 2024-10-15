39d59cdeab5002c368acbac14eed32fc4269749e
Robin Burri <robinburri@tutanota.com>
2024-10-15 14:29:53 +0200

    add RouterLink

--- frontend/AngularApp/src/app/login/login-form/login-form.component.ts
+++ frontend/AngularApp/src/app/login/login-form/login-form.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CustomButtonComponent, FormsModule],
+  imports: [CustomButtonComponent, FormsModule, RouterLink],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
