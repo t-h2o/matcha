fc05b81ae8988e0fd60f8c8a0c5bc49dd8bac968
Robin Burri <robinburri@tutanota.com>
2024-10-15 13:41:34 +0200

    add hover and active css on btn

--- frontend/AngularApp/src/app/login/login-form/login-form.component.ts
+++ frontend/AngularApp/src/app/login/login-form/login-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [CustomButtonComponent],
+  imports: [CustomButtonComponent, FormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
