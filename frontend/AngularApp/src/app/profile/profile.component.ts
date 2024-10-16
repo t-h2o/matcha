3a963d508662b1827411e0547e38747eabee0480
Robin Burri <robinburri@tutanota.com>
2024-10-16 21:04:41 +0200

    refactor register profile

--- frontend/AngularApp/src/app/profile/profile.component.ts
+++ frontend/AngularApp/src/app/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../UI/card/card.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
