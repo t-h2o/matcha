f6f2ae90646423b558b4052f007920949ce21abc
Robin Burri <robinburri@tutanota.com>
2024-10-17 16:44:49 +0200

    shorter checks, and uniform json response with http codes

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,10 @@
-"""Flask, psycopg2, os.environ, contextmanager"""
-
 from os import environ
 from contextlib import contextmanager
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, request, jsonify
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor
 from psycopg2.errors import UndefinedTable
-from werkzeug.security import generate_password_hash
-from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -19,18 +14,11 @@ CORS(app, origins="http://localhost:4200")
 @contextmanager
 def get_db_connection():
     """Generator of database connection"""
-
     conn = connect(environ["DATABASE_URL"])
     try:
         yield conn
     finally:
         conn.close()
-
-
-@app.route("/")
-def hello_world():
-    """Simple title"""
-    return "<h1>Hello, World!</h1>"
 
 
 @app.route("/create")
@@ -54,42 +42,52 @@ def create_table_users():
             )
             conn.commit()
 
-    return jsonify({"succefull": "table users created"})
+    return jsonify({"success": "Table 'users' created"}), 201
 
 
 @app.route("/login", methods=["POST"])
 def login_user():
     """Check the login"""
-    json = request.json
+    if request.headers.get("Content-Type") != "application/json":
+        return jsonify({"error": "Content-Type not supported!"}), 415
 
-    try:
-        username = json["username"]
-        password = json["password"]
-    except KeyError as e:
-        return jsonify({"error": f"{e} is required."})
+    user_data = request.json
+    if user_data is None:
+        return (
+            jsonify({"error": "Invalid JSON data or empty request body"}),
+            400,
+        )
 
-    error = None
+    required_fields = ["username", "password"]
+    missing_fields = [
+        field
+        for field in required_fields
+        if field not in user_data or user_data[field] is None
+    ]
 
-    if not username:
-        error = "Username is required."
-    elif not password:
-        error = "Password is required."
+    if missing_fields:
+        return (
+            jsonify(
+                {
+                    "error": f"The following fields are required and cannot be empty: {', '.join(missing_fields)}"
+                }
+            ),
+            400,
+        )
 
-    if error is not None:
-        return jsonify({"error": error})
+    username = user_data["username"]
+    password = user_data["password"]
 
     with get_db_connection() as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT * FROM users WHERE username = %s", (username,))
             user_db = cur.fetchone()
 
-            if user_db is None:
-                return jsonify({"error": "Incorrect username"})
-            if check_password_hash(user_db[6], password):
-                return jsonify({"success": "loged"})
-            return jsonify({"error": "Incorrect password"})
-
-    return "login"
+    if user_db is None:
+        return jsonify({"error": "Incorrect credentials"}), 401
+    if check_password_hash(user_db[6], password):
+        return jsonify({"success": "Logged in"}), 200
+    return jsonify({"error": "Incorrect credentials"}), 401
 
 
 @app.route("/register", methods=["POST"])
@@ -99,88 +97,94 @@ def register_user():
     Validates that the username is not already taken.
     Hashes the password for security.
     """
-    content_type = request.headers.get("Content-Type")
-    if content_type != "application/json":
-        return jsonify({"error": "Content-Type not supported!"})
+    if request.headers.get("Content-Type") != "application/json":
+        return jsonify({"error": "Content-Type not supported!"}), 415
 
-    json = request.json
+    user_data = request.json
+    if user_data is None:
+        return (
+            jsonify({"error": "Invalid JSON data or empty request body"}),
+            400,
+        )
 
-    try:
-        username = json["username"]
-        password = json["password"]
-        firstname = json["firstname"]
-        lastname = json["lastname"]
-        email = json["email"]
-    except KeyError as e:
-        return jsonify({"error": f"{e} is required."})
+    required_fields = ["username", "password", "firstname", "lastname", "email"]
+    missing_fields = [
+        field
+        for field in required_fields
+        if field not in user_data or user_data[field] is None
+    ]
 
-    error = None
-
-    if not username:
-        error = "Username is required."
-    elif not password:
-        error = "Password is required."
-    elif not firstname:
-        error = "Firstname is required."
-    elif not lastname:
-        error = "Lastname is required."
-    elif not email:
-        error = "Email is required."
-
-    if error is not None:
-        return jsonify({"error": error})
+    if missing_fields:
+        return (
+            jsonify(
+                {
+                    "error": f"The following fields are required and cannot be empty: {', '.join(missing_fields)}"
+                }
+            ),
+            400,
+        )
 
     with get_db_connection() as conn:
         try:
             with conn.cursor() as cur:
                 cur.execute(
-                    "INSERT INTO users (username, password, firstname, lastname, email) VALUES (%s,%s,%s,%s,%s);",
+                    "INSERT INTO users (username, password, firstname, lastname, email) VALUES (%s, %s, %s, %s, %s);",
                     (
-                        username,
-                        generate_password_hash(password),
-                        firstname,
-                        lastname,
-                        email,
+                        user_data["username"],
+                        generate_password_hash(user_data["password"]),
+                        user_data["firstname"],
+                        user_data["lastname"],
+                        user_data["email"],
                     ),
                 )
-                conn.commit()
+            conn.commit()
         except conn.IntegrityError:
-            return jsonify({"error": f"User {username} is already registered."})
+            return (
+                jsonify(
+                    {
+                        "error": f"User {user_data['username']} is already registered."
+                    }
+                ),
+                409,
+            )
+        except Exception as e:
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
-    return jsonify({"succefull": f"User {username} was succefull added"})
+    return (
+        jsonify(
+            {"success": f"User {user_data['username']} was successfully added"}
+        ),
+        201,
+    )
 
 
 @app.route("/drop", methods=["POST"])
 def drop_table():
     """Drop table name in JSON"""
 
-    content_type = request.headers.get("Content-Type")
-    if content_type != "application/json":
-        return jsonify({"error": "Content-Type not supported!"})
-    json = request.json
-    table = json["table"]
+    if request.headers.get("Content-Type") != "application/json":
+        return jsonify({"error": "Content-Type not supported!"}), 415
 
-    error = None
-
+    json_data = request.json
+    table = json_data.get("table")
     if not table:
-        error = "table is required."
-
-    if error is not None:
-        return jsonify({"error": error})
+        return jsonify({"error": "Table is required"}), 400
 
     with get_db_connection() as conn:
         try:
-            cur = conn.cursor()
-            cur.execute(f"DROP table IF EXISTS {table}")
-            conn.commit()
-            return jsonify({"success": f'Table "{table}" was succefull dropped'})
+            with conn.cursor() as cur:
+                cur.execute(f"DROP table IF EXISTS {table}")
+                conn.commit()
+                return (
+                    jsonify(
+                        {"success": f"Table {table} was successfully dropped"}
+                    ),
+                    200,
+                )
         except UndefinedTable:
-            error = "undefined table"
-
+            return jsonify({"error": "Undefined table"}), 400
         except Exception as e:
-            error = e.__class__
-
-    return jsonify({"error": error})
+            return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
