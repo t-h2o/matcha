8f28a0d35806c62e141250d93156fcc1199dca85
Robin Burri <robinburri@tutanota.com>
2024-10-20 16:39:12 +0200

    create dummyData and Pictures

--- frontend/AngularApp/src/app/profile/profile.component.ts
+++ frontend/AngularApp/src/app/profile/profile.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CardComponent } from '../UI/card/card.component';
+import { CustomButtonComponent } from '../UI/custom-button/custom-button.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, FormsModule, CustomButtonComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
