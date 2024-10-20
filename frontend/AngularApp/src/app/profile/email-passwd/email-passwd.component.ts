a0e4e1a84c043a72223d8fab4ab85557da7afb9b
Robin Burri <robinburri@tutanota.com>
2024-10-20 16:45:32 +0200

    create all files for profile parts

---
+++ frontend/AngularApp/src/app/profile/email-passwd/email-passwd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-passwd',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-passwd.component.html',
+  styleUrl: './email-passwd.component.scss'
+})
+export class EmailPasswdComponent {
+
+}
---
+++ frontend/AngularApp/src/app/profile/general-profile/general-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './general-profile.component.html',
+  styleUrl: './general-profile.component.scss'
+})
+export class GeneralProfileComponent {
+
+}
