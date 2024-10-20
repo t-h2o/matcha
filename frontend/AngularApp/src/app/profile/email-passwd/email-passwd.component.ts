ad09394c60f932262afcb3303da9f4e63939a497
Robin Burri <robinburri@tutanota.com>
2024-10-20 16:50:51 +0200

    add some imports

--- frontend/AngularApp/src/app/profile/email-passwd/email-passwd.component.ts
+++ frontend/AngularApp/src/app/profile/email-passwd/email-passwd.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
+import { CardComponent } from '../../UI/card/card.component';
 
 @Component({
   selector: 'app-email-passwd',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CustomButtonComponent],
   templateUrl: './email-passwd.component.html',
   styleUrl: './email-passwd.component.scss'
 })
