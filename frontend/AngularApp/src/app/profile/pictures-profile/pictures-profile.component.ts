15774dbd4ad2b81bc46cc5c4169c26c58fe899c0
Robin Burri <robinburri@tutanota.com>
2024-10-20 16:45:32 +0200

    create all files for profile parts

---
+++ frontend/AngularApp/src/app/profile/pictures-profile/pictures-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pictures-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './pictures-profile.component.html',
+  styleUrl: './pictures-profile.component.scss'
+})
+export class PicturesProfileComponent {
+
+}
