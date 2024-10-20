ac59af444a0ad070013111cd79be8c67fe40a29f
Robin Burri <robinburri@tutanota.com>
2024-10-20 16:39:12 +0200

    create dummyData and Pictures

---
+++ frontend/AngularApp/src/app/profile/dummyUserData.ts
@@ -0,0 +1,12 @@
+export let dummyUserData = {
+    firstName: 'Johnny',
+    lastName: 'Appleseed',
+    email: 'JohnnyAppleseed@test.com',
+    gender: 'male',
+    sexualPreference: 'heterosexual',
+    birthDate: '1990-01-01',
+    bio: 'I am a very interesting person. I like to do interesting things and go to interesting places. I am looking for someone who is also interesting.',
+    interests: ['hiking', 'biking', 'swimming', 'reading', 'cooking', 'traveling'],
+    pictures: [ "johnnyAppleseed1", "johnnyAppleseed2", "johnnyAppleseed3", "johnnyAppleseed4", "johnnyAppleseed5" ],
+    profilePic: "johnnyAppleseed1",
+};
