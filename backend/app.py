2216f236bd2d949df5adf7690a1d031d4cfc7459
|°_°| <grivel.theo@protonmail.com>
2024-10-25 15:07:33 +0200

    backend: reorganize code (#23)
    split on 2 files
    - app.py
    - db.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,7 @@
-"""Flask, psycopg2, os.environ, contextmanager"""
+"""Flask, os.environ"""
 
 from os import environ
 from sys import stderr
-from contextlib import contextmanager
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -12,12 +11,14 @@ from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import get_jwt_identity
 
-from psycopg2 import connect
-from psycopg2.extras import RealDictCursor
-from psycopg2.errors import UndefinedTable
-from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
 from flask_cors import CORS
+
+from db import db_create_table_users
+from db import db_register
+from db import db_drop
+from db import db_get_username_password_where_username
+from db import db_get_user_per_id
 
 
 def flaskprint(message):
@@ -31,48 +32,11 @@ CORS(app, origins="http://localhost:4200")
 jwt = JWTManager(app)
 
 
-def get_user_db_per_id(id_user):
-    user_db = None
-
-    with get_db_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM users WHERE id = %s", (id_user,))
-            user_db = cur.fetchone()
-
-    return user_db
-
-
-@contextmanager
-def get_db_connection():
-    """Generator of database connection"""
-
-    conn = connect(environ["DATABASE_URL"])
-    try:
-        yield conn
-    finally:
-        conn.close()
-
-
 @app.route("/create")
 def create_table_users():
     """Create the Users's table."""
 
-    with get_db_connection() as conn:
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute(
-                """
-                CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                username VARCHAR(12) UNIQUE NOT NULL,
-                firstname VARCHAR NOT NULL,
-                lastname VARCHAR NOT NULL,
-                email VARCHAR NOT NULL,
-                created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-                password VARCHAR NOT NULL
-                );
-                """
-            )
-            conn.commit()
+    db_create_table_users()
 
     return jsonify({"success": "Table 'users' created"}), 201
 
@@ -100,17 +64,16 @@ def login_user():
     username = json["username"]
     password = json["password"]
 
-    with get_db_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM users WHERE username = %s", (username,))
-            user_db = cur.fetchone()
+    user_db = db_get_username_password_where_username(username)
 
-            if user_db is None:
-                return jsonify({"error": "Incorrect username"}), 401
-            if check_password_hash(user_db[6], password):
-                access_token = create_access_token(identity=user_db[0])
-                return jsonify(access_token=access_token)
-            return jsonify({"error": "Incorrect password"}), 401
+    flaskprint(user_db)
+
+    if user_db is None:
+        return jsonify({"error": "Incorrect username"}), 401
+    if check_password_hash(user_db[1], password):
+        access_token = create_access_token(identity=user_db[0])
+        return jsonify(access_token=access_token)
+    return jsonify({"error": "Incorrect password"}), 401
 
 
 @app.route("/who_am_i", methods=["GET"])
@@ -119,7 +82,7 @@ def protected():
     # We can now access our sqlalchemy User object via `current_user`.
     user_id = get_jwt_identity()
     flaskprint(user_id)
-    user_db = get_user_db_per_id(user_id)
+    user_db = db_get_user_per_id(user_id)
     return jsonify(
         id=user_db[0],
         firstname=user_db[1],
@@ -160,24 +123,9 @@ def register_user():
     lastname = json["lastname"]
     email = json["email"]
 
-    with get_db_connection() as conn:
-        try:
-            with conn.cursor() as cur:
-                cur.execute(
-                    "INSERT INTO users (username, password, firstname, lastname, email) VALUES (%s,%s,%s,%s,%s);",
-                    (
-                        username,
-                        generate_password_hash(password),
-                        firstname,
-                        lastname,
-                        email,
-                    ),
-                )
-                conn.commit()
-        except conn.IntegrityError:
-            return jsonify({"error": f"User {username} is already registered."})
+    response = db_register(username, password, firstname, lastname, email)
 
-    return jsonify({"succefull": f"User {username} was succefull added"})
+    return jsonify(response)
 
 
 @app.route("/drop", methods=["POST"])
@@ -197,17 +145,9 @@ def drop_table():
     if error is not None:
         return jsonify({"error": error})
 
-    with get_db_connection() as conn:
-        try:
-            cur = conn.cursor()
-            cur.execute(f"DROP table IF EXISTS {table}")
-            conn.commit()
-            return jsonify({"success": f"Table {table} was successfully dropped"})
-        except UndefinedTable:
-            return jsonify({"error": "Undefined table"}), 400
+    response = db_drop(table)
 
-        except Exception as e:
-            return jsonify({"error": str(e)}), 500
+    return jsonify(response)
 
 
 if __name__ == "__main__":
---
+++ backend/db.py
@@ -0,0 +1,99 @@
+"""Flask, psycopg2, os.environ, contextmanager"""
+
+from os import environ
+from contextlib import contextmanager
+
+from psycopg2 import connect
+from psycopg2.extras import RealDictCursor
+from psycopg2.errors import UndefinedTable
+from werkzeug.security import generate_password_hash
+
+
+@contextmanager
+def get_db_connection():
+    """Generator of database connection"""
+
+    conn = connect(environ["DATABASE_URL"])
+    try:
+        yield conn
+    finally:
+        conn.close()
+
+
+def db_create_table_users():
+    """Create the Users's table."""
+
+    with get_db_connection() as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(
+                """
+                CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(12) UNIQUE NOT NULL,
+                firstname VARCHAR NOT NULL,
+                lastname VARCHAR NOT NULL,
+                email VARCHAR NOT NULL,
+                created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+                password VARCHAR NOT NULL
+                );
+                """
+            )
+            conn.commit()
+
+
+def db_get_username_password_where_username(username):
+    user_db = None
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT username,password FROM users WHERE username = %s", (username,)
+            )
+            user_db = cur.fetchone()
+    return user_db
+
+
+def db_get_user_per_id(id_user):
+    user_db = None
+
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM users WHERE id = %s", (id_user,))
+            user_db = cur.fetchone()
+
+    return user_db
+
+
+def db_register(username, password, firstname, lastname, email):
+    with get_db_connection() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "INSERT INTO users (username, password, firstname, lastname, email) VALUES (%s,%s,%s,%s,%s);",
+                    (
+                        username,
+                        generate_password_hash(password),
+                        firstname,
+                        lastname,
+                        email,
+                    ),
+                )
+                conn.commit()
+        except conn.IntegrityError:
+            return {"error": f"User {username} is already registered."}
+
+    return {"success": f"User {username} was successfully added"}
+
+
+def db_drop(table):
+
+    with get_db_connection() as conn:
+        try:
+            cur = conn.cursor()
+            cur.execute(f"DROP table IF EXISTS {table}")
+            conn.commit()
+        except UndefinedTable:
+            {"error": "Undefined table"}
+        except Exception as e:
+            {"error": str(e)}
+
+    return {"success": f"Table {table} was successfully dropped"}
