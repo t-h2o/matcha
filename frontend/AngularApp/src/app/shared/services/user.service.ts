ca77c1bc909eb715f25eb8becdc005bc1586f311
Robin Burri <robinburri@tutanota.com>
2024-10-26 08:55:03 +0200

    add new UserService method

--- frontend/AngularApp/src/app/shared/services/user.service.ts
+++ frontend/AngularApp/src/app/shared/services/user.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { UserLogin, UserRegister } from '../models/data-to-api/user';
+import { UserLogin, UserModifyGeneral, UserRegister } from '../models/data-to-api/user';
 import { token } from '../models/token';
 
 @Injectable({
@@ -17,5 +17,9 @@ export class UserService {
 
   register(userData: UserRegister) {
     return this.httpClient.post(`${this.baseUrl}/register`, userData);
+  }
+
+  modifyGeneral(userData: UserModifyGeneral) {
+    return this.httpClient.put(`${this.baseUrl}/modify-general`, userData);
   }
 }
