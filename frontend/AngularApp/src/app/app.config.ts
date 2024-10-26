c1160965ce0d5becf24699fc5eb2e97d64869da7
Robin Burri <robinburri@tutanota.com>
2024-10-26 18:21:51 +0200

    add token interceptor

--- frontend/AngularApp/src/app/app.config.ts
+++ frontend/AngularApp/src/app/app.config.ts
@@ -2,12 +2,31 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+
+const tokenInterceptor = (
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn,
+) => {
+  const req = request.clone({
+    headers: request.headers.set(
+      'Authorization',
+      'Bearer ' + localStorage.getItem('access_token'),
+    ),
+  });
+  console.log('Request:', req.headers);
+  return next(req);
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
   ],
 };
