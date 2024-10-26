661287f42ed4d335ebeb2e2c1b55e386646c9d35
Robin Burri <robinburri@tutanota.com>
2024-10-26 18:28:34 +0200

    add a if token

--- frontend/AngularApp/src/app/app.config.ts
+++ frontend/AngularApp/src/app/app.config.ts
@@ -13,11 +13,12 @@ const tokenInterceptor = (
   request: HttpRequest<unknown>,
   next: HttpHandlerFn,
 ) => {
+  const token = localStorage.getItem('access_token');
+  if (!token) {
+    return next(request);
+  }
   const req = request.clone({
-    headers: request.headers.set(
-      'Authorization',
-      'Bearer ' + localStorage.getItem('access_token'),
-    ),
+    headers: request.headers.set('Authorization', 'Bearer ' + token),
   });
   console.log('Request:', req);
   return next(req);
