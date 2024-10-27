384d390aa945d962442735df977a03eef5a71029
Robin Burri <robinburri@tutanota.com>
2024-10-27 09:26:23 +0100

    add sendUserEmailToAPI, sendUserPasswordToAPI methods

--- frontend/AngularApp/src/app/profile/modify-email/modify-email.component.ts
+++ frontend/AngularApp/src/app/profile/modify-email/modify-email.component.ts
@@ -1,8 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CardComponent } from '../../UI/card/card.component';
 import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
 import { PasswordConfirmValidatorDirective } from '../../shared/directives/password-confirm-validator.directive';
+import { finalize } from 'rxjs';
+import { UserModifyEmail, UserModifyPassword } from '../../shared/models/data-to-api/user';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-modify-email',
@@ -19,12 +22,12 @@ import { PasswordConfirmValidatorDirective } from '../../shared/directives/passw
 export class ModifyEmailComponent implements OnInit {
   @Input({ required: true }) onCancel!: () => void;
   @Input({ required: true }) userEmail!: string;
+  private userService = inject(UserService);
 
   uEmail: string = '';
 
   ngOnInit(): void {
     this.uEmail = this.userEmail;
-    console.log('Email:', this.userEmail);
   }
 
   onSubmitEmail(form: NgForm) {
@@ -35,5 +38,41 @@ export class ModifyEmailComponent implements OnInit {
   onSubmitPassword(form: NgForm) {
     console.log('Form Submitted!', form.value);
     this.onCancel();
+  }
+
+  private sendUserEmailToAPI(userData: UserModifyEmail) {
+    const subscription = this.userService
+      .modifyEmail(userData)
+      .pipe(
+        finalize(() => {
+          subscription.unsubscribe();
+        })
+      )
+      .subscribe({
+        next: (data: any) => {
+          console.log('data: ' + JSON.stringify(data));
+        },
+        error: (error: any) => {
+          console.error('error: ' + JSON.stringify(error));
+        },
+      });
+  }
+
+  private sendUserPasswordToAPI(userData: UserModifyPassword) {
+    const subscription = this.userService
+      .modifyPassword(userData)
+      .pipe(
+        finalize(() => {
+          subscription.unsubscribe();
+        })
+      )
+      .subscribe({
+        next: (data: any) => {
+          console.log('data: ' + JSON.stringify(data));
+        },
+        error: (error: any) => {
+          console.error('error: ' + JSON.stringify(error));
+        },
+      });
   }
 }
