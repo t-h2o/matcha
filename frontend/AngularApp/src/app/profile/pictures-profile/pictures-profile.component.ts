3ff7a47da16529481f528cdf3ac97c2dffa1f452
Robin Burri <robinburri@tutanota.com>
2024-10-27 20:05:37 +0100

    add border on profile picture

--- frontend/AngularApp/src/app/profile/pictures-profile/pictures-profile.component.ts
+++ frontend/AngularApp/src/app/profile/pictures-profile/pictures-profile.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CardComponent } from '../../UI/card/card.component';
 import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-pictures-profile',
   standalone: true,
-  imports: [CardComponent, CustomButtonComponent],
+  imports: [CardComponent, CustomButtonComponent, NgClass],
   templateUrl: './pictures-profile.component.html',
   styleUrl: './pictures-profile.component.scss',
 })
