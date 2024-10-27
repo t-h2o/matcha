e23936b0a5c5512f6380531b41e0dec3012421ec
Robin Burri <robinburri@tutanota.com>
2024-10-27 20:58:56 +0100

    change table creation order

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -10,21 +10,19 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
--- one-to-one relationship with users: we can use user_id as PRIMARY KEY
+CREATE TABLE IF NOT EXISTS user_images (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    image_url VARCHAR NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+);
+
 CREATE TABLE IF NOT EXISTS user_profiles (
     user_id INTEGER PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
     bio TEXT,
     gender CHAR(1),
     sexual_orientation CHAR(1),
     profile_picture_id INTEGER REFERENCES user_images(id) ON DELETE SET NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
-);
-
--- one-to-many relationship with users: need id PRIMARY KEY
-CREATE TABLE IF NOT EXISTS user_images (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    image_url VARCHAR NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
@@ -58,26 +56,3 @@ CREATE TABLE IF NOT EXISTS user_likes (
     CONSTRAINT unique_like UNIQUE(liker_id, liked_id),
     CONSTRAINT no_self_likes CHECK (liker_id != liked_id)
 );
-
-
--- populate interests table
-INSERT INTO interests (name) VALUES 
-('travel'),
-('fitness'),
-('music'),
-('photography'),
-('gaming'),
-('yoga'),
-('reading'),
-('movies'),
-('cooking'),
-('hiking'),
-('technology'),
-('fashion'),
-('nature'),
-('meditation'),
-('tattoos'),
-('cats'),
-('dogs'),
-('dance')
-ON CONFLICT (name) DO NOTHING;
