57c2a3d71182e8680c1b5f1af34b6fbce56be2ac
|°_°| <grivel.theo@protonmail.com>
2024-10-31 20:41:30 +0100

    app: split endpoint with function

--- backend/app.py
+++ backend/app.py
@@ -43,24 +43,30 @@ CORS(app, origins="http://localhost:4200")
 jwt = JWTManager(app)
 
 
+def interests_put(id_user, request):
+    json = request.json
+
+    check_request = check_request_json_values(
+        request.headers.get("Content-Type"),
+        json,
+        ["interests"],
+    )
+
+    if check_request is not None:
+        return jsonify(check_request[0]), check_request[1]
+
+    db_set_interests(id_user, json["interests"])
+
+
 @app.route("/api/interests", methods=("PUT", "GET"))
 @jwt_required()
 def interests():
     id_user = get_jwt_identity()
 
     if request.method == "PUT":
-        json = request.json
-
-        check_request = check_request_json_values(
-            request.headers.get("Content-Type"),
-            json,
-            ["interests"],
-        )
-
-        if check_request is not None:
-            return jsonify(check_request[0]), check_request[1]
-
-        db_set_interests(id_user, json["interests"])
+        error_msg = interests_put(id_user, request)
+        if error_msg:
+            return error_msg
 
     interests = db_get_interests(id_user)
 
