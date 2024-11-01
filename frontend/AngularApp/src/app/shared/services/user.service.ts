dbee3bf67b23ec246220b1dc1b21a50190f2aa8c
Robin Burri <robinburri@tutanota.com>
2024-11-01 13:00:42 +0100

    create getInterests and modifyInterests in user.service

--- frontend/AngularApp/src/app/shared/services/user.service.ts
+++ frontend/AngularApp/src/app/shared/services/user.service.ts
@@ -1,6 +1,45 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { UserRequestsService } from './user.requests.service';
+import { finalize } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {}
+export class UserService {
+  private userRequestsService = inject(UserRequestsService);
+
+  interestList = signal<{ interests: string[] }>({ interests: [] });
+
+  getInterests() {
+    const subscription = this.userRequestsService
+      .getInterests()
+      .pipe(finalize(() => subscription.unsubscribe()))
+      .subscribe({
+        next: (data: { interests: string[] }) => {
+          this.interestList.set(data);
+          console.log('Interests from back :', data);
+        },
+        error: (error: any) => {
+          console.log('Error getting interests:', error);
+        },
+      });
+  }
+
+  modifyInterests(selectedTagsObj: { interests: string[] }) {
+    const subscription = this.userRequestsService
+      .modifyInterests(selectedTagsObj)
+      .pipe(
+        finalize(() => {
+          subscription.unsubscribe();
+        }),
+      )
+      .subscribe({
+        next: (data: any) => {
+          console.log('data: ' + JSON.stringify(data));
+        },
+        error: (error: any) => {
+          console.log('Error updating interests:', error);
+        },
+      });
+  }
+}
