d5d3e1b3e56485e35c9ef5f5f6afb694f778bff1
Robin Burri <robinburri@tutanota.com>
2024-11-01 12:41:47 +0100

    userService -> userRequestsService

--- frontend/AngularApp/src/app/shared/services/user.service.ts
+++ frontend/AngularApp/src/app/shared/services/user.service.ts
@@ -1,61 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import {
-  UserLogin,
-  ModifiedUserEmail,
-  ModifiedUserGeneral,
-  ModifiedUserPassword,
-  UserRegister,
-} from '../models/data-to-api/user';
-import { token } from '../models/token';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {
-  private httpClient = inject(HttpClient);
-  private baseUrl = environment.apiUrl;
-
-  login(loginData: UserLogin) {
-    return this.httpClient.post<token>(`${this.baseUrl}/login`, loginData);
-  }
-
-  register(userData: UserRegister) {
-    return this.httpClient.post(`${this.baseUrl}/register`, userData);
-  }
-
-  modifyGeneral(userData: ModifiedUserGeneral) {
-    return this.httpClient.put(`${this.baseUrl}/modify-general`, userData);
-  }
-
-  modifyEmail(userData: ModifiedUserEmail) {
-    return this.httpClient.put(`${this.baseUrl}/modify-email`, userData);
-  }
-
-  modifyPassword(userData: ModifiedUserPassword) {
-    return this.httpClient.put(`${this.baseUrl}/modify-password`, userData);
-  }
-
-  modifyInterests(userData: { interests: string[] }) {
-    return this.httpClient.put(`${this.baseUrl}/interests`, userData);
-  }
-
-  modifyPictures(pictures: File[]) {
-    const formData = new FormData();
-    pictures.forEach((picture) => formData.append('pictures', picture));
-    return this.httpClient.post(`${this.baseUrl}/modify-pictures`, formData);
-  }
-
-  modifyProfilePicture(selectedPictures: string) {
-    return this.httpClient.put(`${this.baseUrl}/modify-profile-picture`, {
-      selectedPictures,
-    });
-  }
-
-  getInterests() {
-    return this.httpClient.get<{ interests: string[] }>(
-      `${this.baseUrl}/interests`,
-    );
-  }
-}
+export class UserService {}
