ac2c3c7e535e0d660e01146b6f5a1cbc2b11bc37
Robin Burri <robinburri@tutanota.com>
2024-11-01 14:42:06 +0100

    modify-general.component use userService to update profile + auto refresh on save

--- frontend/AngularApp/src/app/profile/general-profile/general-profile.component.ts
+++ frontend/AngularApp/src/app/profile/general-profile/general-profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
 import { CardComponent } from '../../UI/card/card.component';
 import { UserData } from '../dummyUserData';
 import { UpperCasePipe } from '@angular/common';
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-general-profile',
@@ -12,25 +13,26 @@ import { UpperCasePipe } from '@angular/common';
   styleUrl: './general-profile.component.scss',
 })
 export class GeneralProfileComponent {
-  @Input() userProfile!: UserData;
   @Input() onModify!: () => void;
+  private userServices = inject(UserService);
+  userProfile = this.userServices.profileData;
 
   onClickModify() {
     this.onModify();
   }
 
   get sexualPreference() {
-    if (this.userProfile.sexualPreference === 'e') {
+    if (this.userProfile().sexualPreference === 'e') {
       return 'Heterosexual';
     }
-    if (this.userProfile.sexualPreference === 'o') {
+    if (this.userProfile().sexualPreference === 'o') {
       return 'Homosexual';
     }
     return 'Bisexual';
   }
 
   get gender() {
-    if (this.userProfile.gender === 'm') {
+    if (this.userProfile().gender === 'm') {
       return 'Male';
     }
     return 'Female';
