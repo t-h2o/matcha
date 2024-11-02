a119d3db86c333ac1f4debf200c8b75139d91191
Robin Burri <robinburri@tutanota.com>
2024-11-02 16:29:02 +0100

    fix rebase

--- frontend/AngularApp/src/app/profile/interests/interests.component.ts
+++ frontend/AngularApp/src/app/profile/interests/interests.component.ts
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Component, computed, inject, Input } from '@angular/core';
-=======
-import {
-  Component,
-  computed,
-  effect,
-  inject,
-  input,
-  Input,
-} from '@angular/core';
->>>>>>> 71acfe7 (components use userService for interests CRUD)
-=======
-import { Component, computed, inject, Input } from '@angular/core';
->>>>>>> 63faf3b (format)
 import { CardComponent } from '../../UI/card/card.component';
 import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
 import { UserService } from '../../shared/services/user.service';
