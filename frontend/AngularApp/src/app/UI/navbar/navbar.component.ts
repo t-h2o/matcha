29209db291e61c595050ae6db7c567e5474e9d66
Robin Burri <robinburri@tutanota.com>
2024-11-02 09:59:58 +0100

    add icon in navbar

--- frontend/AngularApp/src/app/UI/navbar/navbar.component.ts
+++ frontend/AngularApp/src/app/UI/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 
@@ -13,6 +13,9 @@ export class NavbarComponent {
   @Input() onLogout!: () => void;
   authService = inject(AuthService);
   router = inject(Router);
+
+  hasNewNotifications = signal<boolean>(true);
+
   isLoginRoute(): boolean {
     return this.router.url === '/login';
   }
