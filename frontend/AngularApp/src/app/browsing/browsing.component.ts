faa18240a937288c0b65afb7c6a01d401ef56be3
Robin Burri <robinburri@tutanota.com>
2024-11-02 16:16:54 +0100

    wip

--- frontend/AngularApp/src/app/browsing/browsing.component.ts
+++ frontend/AngularApp/src/app/browsing/browsing.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../UI/card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-browsing',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, FormsModule],
   templateUrl: './browsing.component.html',
   styleUrl: './browsing.component.scss',
 })
-export class BrowsingComponent {}
+export class BrowsingComponent {
+  maxAgeGap = signal<number>(0);
+  maxDistance = signal<number>(0);
+  maxFameGap = signal<number>(0);
+
+  getDistanceLabel(): string {
+    return this.maxDistance() >= 101 ? '100+' : this.maxDistance().toString();
+  }
+  
+}
