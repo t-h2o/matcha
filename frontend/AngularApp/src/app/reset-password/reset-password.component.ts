cda0d49fe14a4176d7b23728da92cd3fc37d1cf4
Robin Burri <robinburri@tutanota.com>
2024-11-04 15:56:56 +0100

    navigate to login after submit

--- frontend/AngularApp/src/app/reset-password/reset-password.component.ts
+++ frontend/AngularApp/src/app/reset-password/reset-password.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserService } from '../shared/services/user.service';
 import { CardComponent } from '../UI/card/card.component';
 import { CustomButtonComponent } from '../UI/custom-button/custom-button.component';
@@ -9,17 +10,19 @@ import { CustomButtonComponent } from '../UI/custom-button/custom-button.compone
   standalone: true,
   imports: [CardComponent, FormsModule, CustomButtonComponent],
   templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.scss',
+  styleUrl: './reset-password.component.scss'
 })
 export class ResetPasswordComponent {
   private userService = inject(UserService);
+  private router = inject(Router);
   falseCredentials = signal<boolean>(false);
 
   onSubmit(formData: NgForm) {
-    const loginData: { username: string } = {
+    const loginData: { username: string} = {
       username: formData.value.username,
     };
     this.userService.resetPassword(loginData);
+    this.router.navigate(['/login']);
     formData.form.reset();
   }
 }
