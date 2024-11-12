5f46c4d211dd8e8b793a66c2eaf9cb76ca71c3e4
Robin Burri <robinburri@tutanota.com>
2024-11-12 17:55:13 +0100

    create pictures component

--- frontend/AngularApp/src/app/browsing/user-detail/pictures/pictures.component.ts
+++ frontend/AngularApp/src/app/browsing/user-detail/pictures/pictures.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { PotentialMatchService } from '../../../shared/services/potentialMatch.service';
+import { CardComponent } from '../../../UI/card/card.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-pictures',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, NgClass],
   templateUrl: './pictures.component.html',
   styleUrl: './pictures.component.scss',
 })
-export class PicturesComponent {}
+export class PicturesComponent {
+  private matchService = inject(PotentialMatchService);
+  userPictures = computed(() => this.matchService.otherProfileData().pictures);
+}
--- frontend/AngularApp/src/app/shared/models/emptyUser.ts
+++ frontend/AngularApp/src/app/shared/models/emptyUser.ts
@@ -27,4 +27,5 @@ export const emptyOtherUser = {
   pictures: [],
   urlProfile: '',
   fameRating: 0,
+  interests: [],
 };
