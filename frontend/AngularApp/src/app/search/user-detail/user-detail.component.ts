e0ec5085a29d9ae5bf248884ad1c4b05a16d4c9f
Robin Burri <robinburri@tutanota.com>
2024-11-13 13:22:57 +0100

    Css fix (#79)
    * add heart and fake icon on profile detail
    
    * profile css imporved

--- frontend/AngularApp/src/app/search/user-detail/user-detail.component.ts
+++ frontend/AngularApp/src/app/search/user-detail/user-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
 import { PotentialMatchService } from '../../shared/services/potentialMatch.service';
 import { GeneralComponent } from './general/general.component';
 import { MatchInterestsComponent } from './interests/match-interests.component';
 import { PicturesComponent } from './pictures/pictures.component';
 import { CardComponent } from '../../UI/card/card.component';
+import { getFameRatingStars } from '../../shared/utils/displayUtils';
 
 @Component({
   selector: 'app-user-detail',
@@ -21,8 +22,29 @@ export class UserDetailComponent implements OnInit {
   private PotentialMatchService = inject(PotentialMatchService);
   username = input.required<string>();
   user = this.PotentialMatchService.otherProfileData;
+  isLikedByUser = signal<boolean>(true);
 
   ngOnInit(): void {
     this.PotentialMatchService.getUserProfileByUsername(this.username());
+  }
+
+  get heartIcon(): string {
+    return this.isLikedByUser()
+      ? '/icons/heart_minus.svg'
+      : '/icons/heart_plus.svg';
+  }
+
+  get FameRatingStars(): string {
+    // return getFameRatingStars(this.user.fameRating);
+    return getFameRatingStars(3);
+  }
+
+  toggleLike(): void {
+    this.isLikedByUser.set(!this.isLikedByUser());
+  }
+
+  reportAsFake(username: string): void {
+    console.log(`reporting ${username} as fake`);
+    // this.PotentialMatchService.reportUserAsFake(this.user.username);
   }
 }
