def70baa6fc9e2e00282d2a9f47397ab19553dff
@ <@>
2024-11-14 20:13:13 +0100

    backend: socket io

--- backend/matcha/__init__.py
+++ backend/matcha/__init__.py
@@ -2,6 +2,8 @@ from os import environ
 
 from flask import Flask
 
+from flask_socketio import SocketIO
+
 from flask_cors import CORS
 
 from flask_jwt_extended import (
@@ -21,6 +23,9 @@ def create_app():
     app.config["JWT_SECRET_KEY"] = environ["FLASK_JWT_SECRET_KEY"]
     app.config["UPLOAD_FOLDER"] = environ["FLASK_UPLOAD_FOLDER"]
     app.config["URL"] = environ["FLASK_URL"]
+    app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your own secret key
+
+    socketio = SocketIO(app)
 
     CORS(app, origins="http://localhost:4200")
 
@@ -31,5 +36,14 @@ def create_app():
     app.register_blueprint(images.bp)
     app.register_blueprint(interests.bp)
     app.register_blueprint(pictures.bp)
+
+    @socketio.on('connect')
+    def handle_connect():
+        print('Client connected')
+
+    @socketio.on('message')
+    def handle_message(data):
+        print('Received message:', data)
+        socketio.emit('response', 'Server received your message: ' + data)
 
     return app
