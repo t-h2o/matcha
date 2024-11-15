c18e2ece288fa9f20a8d268bd5f759098450ba87
|°_°| <grivel.theo@protonmail.com>
2024-11-15 14:16:53 +0100

    backend: [BUG]: SocketIO
    I really do not understand why
    
    I tried to run the app in the container
    
    docker exec -it flask-dev sh
    
    ```
    /app/matcha # python __init__.py
    Traceback (most recent call last):
      File "/app/matcha/__init__.py", line 37, in <module>
        SocketIO.run(app)
    TypeError: run() missing 1 required positional argument: 'app'
    ```
    
    Resources:
    
    - https://stackoverflow.com/a/57686577
    - https://github.com/miguelgrinberg/Flask-SocketIO/issues/894#issuecomment-460034033
    - https://github.com/willthames/docker-debug/blob/master/server.py
    - https://github.com/miguelgrinberg/Flask-SocketIO
    
    TODO:
    
    Follow and try this guide
    
    - https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
    
    from this repository
    
    - https://github.com/miguelgrinberg/Flask-SocketIO

--- backend/matcha/__init__.py
+++ backend/matcha/__init__.py
@@ -10,20 +10,13 @@ from flask_jwt_extended import (
     JWTManager,
 )
 
-from matcha import users
-from matcha import auth
-from matcha import images
-from matcha import interests
-from matcha import pictures
-
-
-def create_app():
+if __name__ == "__main__":
     app = Flask(__name__)
 
     app.config["JWT_SECRET_KEY"] = environ["FLASK_JWT_SECRET_KEY"]
     app.config["UPLOAD_FOLDER"] = environ["FLASK_UPLOAD_FOLDER"]
     app.config["URL"] = environ["FLASK_URL"]
-    app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your own secret key
+    app.config["SECRET_KEY"] = "your_secret_key"  # Replace with your own secret key
 
     socketio = SocketIO(app)
 
@@ -31,19 +24,13 @@ def create_app():
 
     jwt = JWTManager(app)
 
-    app.register_blueprint(users.bp)
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(images.bp)
-    app.register_blueprint(interests.bp)
-    app.register_blueprint(pictures.bp)
-
-    @socketio.on('connect')
+    @socketio.on("connect")
     def handle_connect():
-        print('Client connected')
+        print("Client connected")
 
-    @socketio.on('message')
+    @socketio.on("message")
     def handle_message(data):
-        print('Received message:', data)
-        socketio.emit('response', 'Server received your message: ' + data)
+        print("Received message:", data)
+        socketio.emit("response", "Server received your message: " + data)
 
-    return app
+    SocketIO.run(app)
