fc378e8171a24bc8fddb2bef716f9d787115c5db
|°_°| <grivel.theo@protonmail.com>
2024-11-18 09:28:26 +0100

    backend: fastapi first example
    https://fastapi.tiangolo.com/#example

--- backend/matcha/__init__.py
+++ backend/matcha/__init__.py
@@ -1,35 +1,15 @@
-from os import environ
+from typing import Union
 
-from flask import Flask
+from fastapi import FastAPI
 
-from flask_cors import CORS
-
-from flask_jwt_extended import (
-    JWTManager,
-)
-
-from matcha import users
-from matcha import auth
-from matcha import images
-from matcha import interests
-from matcha import pictures
+app = FastAPI()
 
 
-def create_app():
-    app = Flask(__name__)
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-    app.config["JWT_SECRET_KEY"] = environ["FLASK_JWT_SECRET_KEY"]
-    app.config["UPLOAD_FOLDER"] = environ["FLASK_UPLOAD_FOLDER"]
-    app.config["URL"] = environ["FLASK_URL"]
 
-    CORS(app, origins="http://localhost:4200")
-
-    jwt = JWTManager(app)
-
-    app.register_blueprint(users.bp)
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(images.bp)
-    app.register_blueprint(interests.bp)
-    app.register_blueprint(pictures.bp)
-
-    return app
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
