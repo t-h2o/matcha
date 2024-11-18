c77841145fe91c3ceba7c938ffd29a5f14c8613d
|°_°| <grivel.theo@protonmail.com>
2024-11-18 10:12:37 +0100

    backend: [BUG] allow origin
    resource:
    - https://fastapi.tiangolo.com/tutorial/cors/#origin
    
    I don't understand, why it doesn't work
    
    fastapi port 8000
    docker compose bridge 8000->5001
    
    $ docker exec flask-dev wget 127.0.0.1:8000  -SO - # inside the container
    Connecting to 127.0.0.1:8000 (127.0.0.1:8000)
      HTTP/1.1 200 OK
      date: Mon, 18 Nov 2024 09:16:30 GMT
      server: uvicorn
      content-length: 17
      content-type: application/json
      connection: close
    
    writing to stdout
    {"Hello":"World"}-
    written to stdout
    
    $ curl http://localhost:5001/docs # outside the container
    curl: (56) Recv failure: Connection reset by peer
    
    $ wget http://localhost:5001/docs -SO -
    --2024-11-18 10:17:18--  http://localhost:5001/docs
    Resolving localhost (localhost)... ::1, 127.0.0.1
    Connecting to localhost (localhost)|::1|:5001... connected.
    HTTP request sent, awaiting response... Read error (Connection reset by peer) in headers.
    Retrying.
    
    ^C

--- backend/matcha/__init__.py
+++ backend/matcha/__init__.py
@@ -2,11 +2,22 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
