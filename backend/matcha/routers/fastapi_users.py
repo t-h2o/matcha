ffb57c1cd7cc3d77d2af766d1cd0e34b52642b7d
|°_°| <grivel.theo@protonmail.com>
2024-11-18 19:39:15 +0100

    wip

--- backend/matcha/routers/fastapi_users.py
+++ backend/matcha/routers/fastapi_users.py
@@ -26,11 +26,6 @@ async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
 
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
-
-
 @router.get("/users/{username}", tags=["users"])
 async def read_user(username: str):
     return {"username": username}
@@ -40,3 +35,38 @@ async def read_user(username: str):
 async def get_token():
     token = create_access_token({"username": "user", "age": 18}, timedelta(minutes=15))
     return {"username": "user", "token": token}
+
+
+async def check_token(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(check_token)],
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)],
+):
+    return current_user
