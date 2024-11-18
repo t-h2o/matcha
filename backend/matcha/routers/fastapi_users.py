c285f059927863195b6e4b7306a0243ea4678d58
@ <@>
2024-11-18 17:42:38 +0100

    [WIP]: JWT

--- backend/matcha/routers/fastapi_users.py
+++ backend/matcha/routers/fastapi_users.py
@@ -1,6 +1,20 @@
 from fastapi import APIRouter
+from datetime import datetime, timedelta, timezone
+import jwt
 
 router = APIRouter()
+
+
+# def create_access_token(data: dict, expires_delta: timedelta | None = None):
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.get("/users/", tags=["users"])
@@ -16,3 +30,9 @@ async def read_user_me():
 @router.get("/users/{username}", tags=["users"])
 async def read_user(username: str):
     return {"username": username}
+
+
+@router.get("/token", tags=["users"])
+async def get_token():
+    token = create_access_token({"username": "user", "age": 18}, timedelta(minutes=15))
+    return {"username": "user", "token": token}
