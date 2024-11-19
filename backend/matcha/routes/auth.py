a9db30063c2fd3be54acbbcc9d1b114566b003c3
|°_°| <grivel.theo@protonmail.com>
2024-11-19 21:28:59 +0100

    services: better split auth

--- backend/matcha/routes/auth.py
+++ backend/matcha/routes/auth.py
@@ -9,18 +9,7 @@ bp = Blueprint("auth", __name__)
 
 @bp.route("/api/login", methods=["POST"])
 def login_user():
-    json = request.json
-
-    check_request = check_request_json(
-        request.headers.get("Content-Type"),
-        json,
-        ["username", "password"],
-    )
-
-    if check_request is not None:
-        return jsonify(check_request[0]), check_request[1]
-
-    return service_login_user(Login(json["username"], json["password"]))
+    return service_login_user(request)
 
 
 @bp.route("/api/reset-password", methods=["POST"])
--- backend/matcha/services/auth.py
+++ backend/matcha/services/auth.py
@@ -8,6 +8,8 @@ from werkzeug.security import check_password_hash
 
 from matcha.db import db_get_id_password_where_username
 
+from matcha.app_utils import check_request_json
+
 
 class Login:
     username: str
@@ -18,12 +20,23 @@ class Login:
         self.password = password
 
 
-def service_login_user(login: Login):
-    user_db = db_get_id_password_where_username(login.username)
+def service_login_user(request):
+    json = request.json
+
+    check_request = check_request_json(
+        request.headers.get("Content-Type"),
+        json,
+        ["username", "password"],
+    )
+
+    if check_request is not None:
+        return jsonify(check_request[0]), check_request[1]
+
+    user_db = db_get_id_password_where_username(json["username"])
 
     if user_db is None:
         return jsonify({"error": "Incorrect username"}), 401
-    if check_password_hash(user_db[1], login.password):
+    if check_password_hash(user_db[1], json["password"]):
         access_token = create_access_token(identity=user_db[0])
         return jsonify(access_token=access_token)
     return jsonify({"error": "Incorrect password"}), 401
