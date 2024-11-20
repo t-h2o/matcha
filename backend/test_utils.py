4fd005fba8276d0b79359d4cd7426108be3d9224
|°_°| <grivel.theo@protonmail.com>
2024-11-20 20:30:17 +0100

    test: position

--- backend/test_utils.py
+++ backend/test_utils.py
@@ -168,6 +168,16 @@ def check_get_token(path, status, content):
     )
 
 
+def check_post_token(path, status, json, content):
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    response = post(URL + path, headers=headers, json=json)
+
+    check_content_code(
+        URL, path, status, response.status_code, content, response.content, json
+    )
+
+
 def check_put_token(path, status, json, content):
     headers = {"Authorization": f"Bearer {access_token}"}
 
