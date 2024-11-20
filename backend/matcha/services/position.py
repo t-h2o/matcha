4f0ea80d11480245330171e39636b4ee6ae88eaa
|°_°| <grivel.theo@protonmail.com>
2024-11-20 19:14:41 +0100

    backend: position

---
+++ backend/matcha/services/position.py
@@ -0,0 +1,26 @@
+from flask import jsonify
+
+from flask_jwt_extended import (
+    create_access_token,
+)
+
+from werkzeug.security import check_password_hash
+
+from matcha.db import db_get_id_password_where_username
+
+from matcha.app_utils import check_request_json
+
+
+def service_position(id_user, request):
+    json = request.json
+
+    check_request = check_request_json(
+        request.headers.get("Content-Type"),
+        json,
+        ["latitude", "longitude"],
+    )
+
+    if check_request is not None:
+        return jsonify(check_request[0]), check_request[1]
+
+    return jsonify(json), 201
