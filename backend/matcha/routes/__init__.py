d967ebc5ab4786d4a476fd010c1499d53760e58d
|°_°| <grivel.theo@protonmail.com>
2024-11-20 19:14:41 +0100

    backend: position

--- backend/matcha/routes/__init__.py
+++ backend/matcha/routes/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 import matcha.routes.users
 import matcha.routes.auth
+import matcha.routes.position
 import matcha.routes.images
 import matcha.routes.interests
 import matcha.routes.pictures
@@ -11,6 +12,7 @@ import matcha.routes.like
 def init_routes(app):
     app.register_blueprint(users.bp)
     app.register_blueprint(auth.bp)
+    app.register_blueprint(position.bp)
     app.register_blueprint(images.bp)
     app.register_blueprint(interests.bp)
     app.register_blueprint(pictures.bp)
