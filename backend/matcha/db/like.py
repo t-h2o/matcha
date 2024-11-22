081288c3db34bdbe202d0a1ee46241f8e66fa1ce
|°_°| <grivel.theo@protonmail.com>
2024-11-22 20:08:01 +0100

    db: get_liker_username

--- backend/matcha/db/like.py
+++ backend/matcha/db/like.py
@@ -7,6 +7,21 @@ from matcha.db.utils import (
 )
 
 
+def db_get_liker_username(id_user):
+    query = """
+    SELECT username FROM users
+    WHERE users.id
+    IN
+    (
+      SELECT liker_id FROM user_likes where liked_id = %s
+    );
+    """
+
+    liker_username = db_fetchall(query, (id_user,))
+
+    return fetchall_to_array(liker_username)
+
+
 def db_put_like_user(id_user, username):
     query = """
     INSERT INTO user_likes
