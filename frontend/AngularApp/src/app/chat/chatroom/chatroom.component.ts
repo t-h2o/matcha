e3c6d958a8032da09b46c4dd1cee5755c120a91e
Robin Burri <robinburri@tutanota.com>
2024-11-24 15:57:39 +0100

    add chat display css

--- frontend/AngularApp/src/app/chat/chatroom/chatroom.component.ts
+++ frontend/AngularApp/src/app/chat/chatroom/chatroom.component.ts
@@ -1,20 +1,64 @@
 import { Component, input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Message } from '../../shared/models/message';
 import { CardComponent } from '../../UI/card/card.component';
+import { DisplayMessagesComponent } from './display-messages/display-messages.component';
+import { CustomButtonComponent } from '../../UI/custom-button/custom-button.component';
+
+const DummyChat = [
+  {
+    id: 101,
+    senderUsername: 'Alice',
+    text: 'Hello Robin!',
+  },
+  {
+    id: 102,
+    senderUsername: 'roburi',
+    text: 'Hi Alice!',
+  },
+  {
+    id: 103,
+    senderUsername: 'Alice',
+    text: 'How are you?',
+  },
+  {
+    id: 104,
+    senderUsername: 'roburi',
+    text: 'I am fine, thank you!',
+  },
+];
 
 @Component({
   selector: 'app-chatroom',
   standalone: true,
-  imports: [CardComponent],
+  imports: [
+    CardComponent,
+    FormsModule,
+    DisplayMessagesComponent,
+    CustomButtonComponent,
+  ],
   templateUrl: './chatroom.component.html',
   styleUrl: './chatroom.component.scss',
 })
 export class ChatroomComponent implements OnInit {
   username = input.required<string>();
+  messages: Message[] = DummyChat;
+  messageText = '';
 
   ngOnInit(): void {
     console.log(
       'ChatroomComponent, loading chatroom for user:',
       this.username(),
     );
+  }
+
+  sendMessage(): void {
+    console.log('ChatroomComponent, sending message:', this.messageText);
+    this.messages.push({
+      id: this.messages.length + 1,
+      senderUsername: this.username(),
+      text: this.messageText,
+    });
+    this.messageText = '';
   }
 }
---
+++ frontend/AngularApp/src/app/chat/chatroom/display-messages/display-messages.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { Message } from '../../../shared/models/message';
+import { UserService } from '../../../shared/services/user.service';
+
+@Component({
+  selector: 'app-display-messages',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './display-messages.component.html',
+  styleUrl: './display-messages.component.scss',
+})
+export class DisplayMessagesComponent {
+  @Input() messages: Message[] = [];
+  private userService = inject(UserService);
+
+  username = this.userService.ownProfileData().username;
+}
