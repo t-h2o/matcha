a8ba1647a3d89a870c31c01877f02db080fc2129
|°_°| <grivel.theo@protonmail.com>
2024-11-25 19:05:54 +0100

    service: position call db

--- backend/matcha/services/position.py
+++ backend/matcha/services/position.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import (
 
 from werkzeug.security import check_password_hash
 
-from matcha.db.db import db_get_id_password_where_username
+from matcha.db.db import db_update_position
 
 from matcha.app_utils import check_request_json
 
@@ -22,5 +22,12 @@ def service_position(id_user, request):
 
     if check_request is not None:
         return jsonify(check_request[0]), check_request[1]
+
+    error_msg = db_update_position(id_user, json["latitude"], json["longitude"])
+    if error_msg is not None:
+
+        if error_msg["error"] == "InvalidTextRepresentation":
+            return jsonify({"error": "bad input"}), 422
+        return jsonify(error_msg), 400
 
     return jsonify(json), 201
