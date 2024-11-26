749ef188c167e144d24090a6bbde30228ef3550e
|°_°| <grivel.theo@protonmail.com>
2024-11-26 12:06:59 +0100

    geoapi: prepare the requests

--- backend/matcha/geoapi/latlon_to_address.py
+++ backend/matcha/geoapi/latlon_to_address.py
@@ -1,4 +1,5 @@
 from xmltodict import parse
+from requests import get
 
 
 def _remove_first_line(text: str) -> str:
@@ -6,8 +7,12 @@ def _remove_first_line(text: str) -> str:
 
 
 def latlon_to_address(lat: float, lon: float) -> dict:
-    # TODO: do the request
-    # curl  https://nominatim.openstreetmap.org/reverse\?lat\=46.532327\&lon\=6.591987
+    api_url = "https://nominatim.openstreetmap.org"
+    path = "/reverse"
+    data = {"lat": lat, "lon": lon}
+
+    # response = get(api_url + path, data=data)
+    # print(response.content)
 
     api_response = """<?xml version="1.0" encoding="UTF-8" ?>
     <reversegeocode timestamp="Mon, 25 Nov 2024 18:57:17 +00:00" attribution="Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright" querystring="lat=46.532327&amp;lon=6.591987&amp;format=xml"><result place_id="84050034" osm_type="node" osm_id="10565205589" ref="42 Lausanne" lat="46.5323286" lon="6.5920061" boundingbox="46.5322786,46.5323786,6.5919561,6.5920561" place_rank="30" address_rank="30">42 Lausanne, 64, Rue de Lausanne, Renens-Village, Renens, District de l'Ouest lausannois, Vaud, 1020, Schweiz/Suisse/Svizzera/Svizra</result><addressparts><amenity>42 Lausanne</amenity><house_number>64</house_number><road>Rue de Lausanne</road><neighbourhood>Renens-Village</neighbourhood><town>Renens</town><county>District de l'Ouest lausannois</county><state>Vaud</state><ISO3166-2-lvl4>CH-VD</ISO3166-2-lvl4><postcode>1020</postcode><country>Schweiz/Suisse/Svizzera/Svizra</country><country_code>ch</country_code></addressparts></reversegeocode>
