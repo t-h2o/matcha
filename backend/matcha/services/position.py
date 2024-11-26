907be3f95faba768bac27906f5783ac446d3d9a2
|°_°| <grivel.theo@protonmail.com>
2024-11-26 18:00:27 +0100

    service: position split function

--- backend/matcha/services/position.py
+++ backend/matcha/services/position.py
@@ -6,14 +6,15 @@ from flask_jwt_extended import (
 
 from werkzeug.security import check_password_hash
 
-from matcha.db.db import db_update_position, db_get_position
+from matcha.db.db import db_update_address, db_update_position, db_get_position
 
 from matcha.app_utils import check_request_json
 
+from matcha.geoapi.latlon_to_address import latlon_to_address
 
-def service_position(id_user, request):
+
+def service_position_put(id_user, request):
     json = request.json
-
     check_request = check_request_json(
         request.headers.get("Content-Type"),
         json,
@@ -29,6 +30,21 @@ def service_position(id_user, request):
         if error_msg["error"] == "InvalidTextRepresentation":
             return jsonify({"error": "bad input"}), 422
         return jsonify(error_msg), 400
+
+    position = db_get_position(id_user)
+
+    address = latlon_to_address(position[0], position[1])
+
+    db_update_address(id_user, address)
+
+
+def service_position(id_user, request):
+    position = None
+
+    if request.method == "POST":
+        error_msg = service_position_put(id_user, request)
+        if error_msg is not None:
+            return error_msg
 
     position = db_get_position(id_user)
 
