57085cb090578cc61164f2c1ae4fa64817d151d1
Robin Burri <robinburri@tutanota.com>
2024-11-28 14:32:33 +0100

    fix sometimes, user profile data not displayed

--- frontend/AngularApp/src/app/app.component.ts
+++ frontend/AngularApp/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { NavbarComponent } from './UI/navbar/navbar.component';
 
 import { AuthService } from './shared/services/auth.service';
 import { ToastComponent } from './shared/toast/toast.component';
-import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -16,13 +15,11 @@ import { UserService } from './shared/services/user.service';
 })
 export class AppComponent implements OnInit {
   private authService = inject(AuthService);
-  private userServices = inject(UserService);
 
   ngOnInit() {
     const token = sessionStorage.getItem('access_token');
     if (token) {
       this.authService.tokenSignal.set({ access_token: token });
-      this.userServices.getUserProfile();
     }
   }
 
