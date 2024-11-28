4031ada724f335bf92adbf0115c78b575defb778
Robin Burri <robinburri@tutanota.com>
2024-11-28 14:44:31 +0100

    fix chat msg display on reload

--- frontend/AngularApp/src/app/chat/chatroom/display-messages/display-messages.component.ts
+++ frontend/AngularApp/src/app/chat/chatroom/display-messages/display-messages.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Message } from '../../../shared/models/message';
 import { UserService } from '../../../shared/services/user.service';
 
@@ -10,9 +10,13 @@ import { UserService } from '../../../shared/services/user.service';
   templateUrl: './display-messages.component.html',
   styleUrl: './display-messages.component.scss',
 })
-export class DisplayMessagesComponent {
+export class DisplayMessagesComponent implements OnInit {
   @Input() messages: Message[] = [];
   private userService = inject(UserService);
+
+  ngOnInit(): void {
+    this.userService.getUserProfile();
+  }
 
   get username(): string {
     return this.userService.ownProfileData().username;
