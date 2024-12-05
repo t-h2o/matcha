db657ebccb4cd046b4197f7ebc8133da45bb4310
Robin Burri <robinburri@tutanota.com>
2024-12-05 20:33:03 +0100

    add notif content

--- frontend/AngularApp/src/app/notification/notification.component.ts
+++ frontend/AngularApp/src/app/notification/notification.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from '../UI/card/card.component';
+
+const DUMMY_NOTIFICATIONS = [
+  { id: 1, title: 'like', text: 'username has liked your profile' },
+  { id: 2, title: 'view', text: 'username has viewed your profile' },
+  { id: 3, title: 'message', text: 'username has send you a message' },
+  { id: 4, title: 'unlike', text: 'username has unlike your profile' },
+  { id: 5, title: 'match', text: 'you and username are a match' },
+];
+
+type Notification = {
+  id: number;
+  title: string;
+  text: string;
+};
 
 @Component({
   selector: 'app-notification',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss',
 })
-export class NotificationComponent {}
+export class NotificationComponent {
+  notificationList = signal<Notification[]>(DUMMY_NOTIFICATIONS);
+}
