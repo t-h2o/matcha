89bc10b85b007061c7061e7d3a6b2f6818e7b7d6
|°_°| <grivel.theo@protonmail.com>
2024-12-05 12:19:14 +0100

    feat(like): call dislike and use get method

--- backend/matcha/services/like.py
+++ backend/matcha/services/like.py
@@ -2,7 +2,7 @@ from flask import jsonify
 
 from matcha.app_utils import check_request_json_values
 
-from matcha.db.like import db_put_like_user, db_get_liker_username
+from matcha.db.like import db_put_dislike_user, db_put_like_user, db_get_liker_username
 
 
 def services_like_user_get(id_user):
@@ -10,18 +10,18 @@ def services_like_user_get(id_user):
 
 
 def services_like_user(id_user, request):
+    if request.method == "GET":
+        likers = services_like_user_get(id_user)
+        return jsonify({"likers": likers}), 201
+
     json = request.json
 
-    check_request = check_request_json_values(
-        request.headers.get("Content-Type"),
-        json,
-        ["username"],
-    )
-
-    if check_request is not None:
-        return jsonify(check_request[0]), check_request[1]
-
-    error = db_put_like_user(id_user, json["username"])
+    if "like" in json:
+        error = db_put_like_user(id_user, json["like"])
+    elif "dislike" in json:
+        error = db_put_dislike_user(id_user, json["dislike"])
+    else:
+        return jsonify({"error": "bad payload"}), 400
 
     if error is not None:
         return error
