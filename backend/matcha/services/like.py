3ad2f2f5adaf43d7742419ed4a56d0dcfcc4e6f5
|°_°| <grivel.theo@protonmail.com>
2024-12-05 19:00:32 +0100

    back(like): return isLiked

--- backend/matcha/services/like.py
+++ backend/matcha/services/like.py
@@ -2,7 +2,12 @@ from flask import jsonify
 
 from matcha.app_utils import check_request_json_values
 
-from matcha.db.like import db_put_dislike_user, db_put_like_user, db_get_liker_username
+from matcha.db.like import (
+    db_put_dislike_user,
+    db_put_like_user,
+    db_get_liker_username,
+    db_get_is_liked,
+)
 
 
 def services_like_user_get(id_user):
@@ -16,9 +21,13 @@ def services_like_user(id_user, request):
 
     json = request.json
 
+    username = None
+
     if "like" in json:
+        username = json["like"]
         error = db_put_like_user(id_user, json["like"])
     elif "dislike" in json:
+        username = json["dislike"]
         error = db_put_dislike_user(id_user, json["dislike"])
     else:
         return jsonify({"error": "bad payload"}), 400
@@ -26,6 +35,5 @@ def services_like_user(id_user, request):
     if error is not None:
         return error
 
-    likers = services_like_user_get(id_user)
-
-    return jsonify({"likers": likers}), 201
+    is_liked = db_get_is_liked(id_user, username)
+    return jsonify({"isLiked": is_liked}), 201
