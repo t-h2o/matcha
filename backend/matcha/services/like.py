0e14fc491604a2a6e43755dced9811fda58438ea
|°_°| <grivel.theo@protonmail.com>
2024-12-06 17:07:20 +0100

    fix(noti): change the user who has the notification

--- backend/matcha/services/like.py
+++ backend/matcha/services/like.py
@@ -46,7 +46,7 @@ def services_like_user(id_user, request):
         content = f"{liker_username} like you"
         notification_message = {"title": title, "content": content}
 
-        db_put_notification(id_user, title, content)
+        db_put_notification(id_to_notify, title, content)
 
         sid = SocketManager().get_sid(id_to_notify[0])
         if sid is not None:
