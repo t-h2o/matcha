dd2896e25b572fcaf9c61be0159693c920640591
Robin Burri <robinburri@tutanota.com>
2024-12-06 09:50:58 +0100

    add date + fix css notif

--- frontend/AngularApp/src/app/notification/notification.component.ts
+++ frontend/AngularApp/src/app/notification/notification.component.ts
@@ -2,17 +2,43 @@ import { Component, signal } from '@angular/core';
 import { CardComponent } from '../UI/card/card.component';
 
 const DUMMY_NOTIFICATIONS = [
-  { id: 1, title: 'like', text: 'username has liked your profile' },
-  { id: 2, title: 'view', text: 'username has viewed your profile' },
-  { id: 3, title: 'message', text: 'username has send you a message' },
-  { id: 4, title: 'unlike', text: 'username has unlike your profile' },
-  { id: 5, title: 'match', text: 'you and username are a match' },
+  {
+    id: 1,
+    title: 'like',
+    text: 'username has liked your profile',
+    date: '2021-09-01',
+  },
+  {
+    id: 2,
+    title: 'view',
+    text: 'username has viewed your profile',
+    date: '2021-09-01',
+  },
+  {
+    id: 3,
+    title: 'message',
+    text: 'username has send you a message',
+    date: '2021-09-01',
+  },
+  {
+    id: 4,
+    title: 'unlike',
+    text: 'username has unlike your profile',
+    date: '2021-09-01',
+  },
+  {
+    id: 5,
+    title: 'match',
+    text: 'you and username are a match',
+    date: '2021-09-01',
+  },
 ];
 
 type Notification = {
   id: number;
   title: string;
   text: string;
+  date: string;
 };
 
 @Component({
