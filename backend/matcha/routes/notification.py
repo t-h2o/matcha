31f33386e1cd348e8b6cea05a88e0c0dea2b5807
|°_°| <grivel.theo@protonmail.com>
2024-12-07 13:06:14 +0100

    back(noti): add route to delete

--- backend/matcha/routes/notification.py
+++ backend/matcha/routes/notification.py
@@ -5,13 +5,23 @@ from flask_jwt_extended import (
     get_jwt_identity,
 )
 
-from matcha.services.notification import services_notification
+from matcha.services.notification import (
+    services_notification,
+    services_delete_notification,
+)
 
 bp = Blueprint("notification", __name__)
 
 
 @bp.route("/api/notification")
 @jwt_required()
-def interests():
+def get_notification():
     id_user = get_jwt_identity()
     return services_notification(id_user)
+
+
+@bp.route("/api/notification/<int:id_notification>/")
+@jwt_required()
+def delete_notification(id_notification):
+    id_user = get_jwt_identity()
+    return services_delete_notification(id_user, id_notification)
