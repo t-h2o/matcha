1e1d0bd8228425e13bd4edebff505dbbeca8de85
|°_°| <grivel.theo@protonmail.com>
2024-12-07 13:05:46 +0100

    back(noti): create service to destroy

--- backend/matcha/services/notification.py
+++ backend/matcha/services/notification.py
@@ -2,9 +2,16 @@ from flask import jsonify
 
 from matcha.db.notification import (
     db_get_notification,
+    db_destroy_notification,
 )
 
 from matcha.app_utils import flaskprint
+
+
+def services_delete_notification(id_user: int, id_notification: int):
+    notifications = db_destroy_notification(id_user, id_notification)
+    flaskprint(notifications)
+    return jsonify({"delete": id_notification}), 201
 
 
 def services_notification(id_user: int):
