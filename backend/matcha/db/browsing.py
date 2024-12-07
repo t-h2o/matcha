d10301b85383fdb94b77d394c22ce2dc753e0037
|°_°| <grivel.theo@protonmail.com>
2024-12-07 15:32:36 +0100

    db(browsing): adapt the query

--- backend/matcha/db/browsing.py
+++ backend/matcha/db/browsing.py
@@ -7,24 +7,44 @@ from matcha.db.utils import (
 )
 
 
-def db_browsing_gender_sexualorientation(id_user, search):
-    return db_fetchall(
-        """
+def _get_query(search) -> str:
+    query = """
         SELECT id, username, firstname, lastname, gender, sexual_orientation, age, fame_rating
         FROM users
         WHERE sexual_orientation = %s
           AND gender = %s
-          AND age BETWEEN %s AND %s
-          AND fame_rating BETWEEN %s AND %s
           AND id != %s
-            """,
-        (
-            search["sexual_orientation"],
-            search["gender"],
-            search["min_age"],
-            search["max_age"],
-            search["min_fame"],
-            search["max_fame"],
-            id_user,
-        ),
-    )
+    """
+
+    if search["max_fame"] is not None:
+        query += " AND fame_rating BETWEEN %s AND %s"
+
+    if search["max_age"] is not None:
+        query += " AND age BETWEEN %s AND %s"
+
+    return query
+
+
+def _get_parameters(id_user: int, search: dict) -> list:
+    parameters = []
+
+    parameters.append(search["sexual_orientation"])
+    parameters.append(search["gender"])
+    parameters.append(id_user)
+
+    if search["max_fame"] is not None:
+        parameters.append(search["min_fame"])
+        parameters.append(search["max_fame"])
+
+    if search["max_age"] is not None:
+        parameters.append(search["min_age"])
+        parameters.append(search["max_age"])
+
+    return list(parameters)
+
+
+def db_browsing_gender_sexualorientation(id_user, search):
+    query = _get_query(search)
+    parameters = _get_parameters(id_user, search)
+
+    return db_fetchall(query, parameters)
