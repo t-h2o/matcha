28b33853a31b3c76d22518784e0fb2b1b7b0a3ec
|°_°| <grivel.theo@protonmail.com>
2024-12-09 14:20:37 +0100

    back(browsing): change payload

--- backend/matcha/services/browsing.py
+++ backend/matcha/services/browsing.py
@@ -9,6 +9,8 @@ from matcha.db.browsing import db_browsing_gender_sexualorientation
 
 from matcha.app_utils import check_request_json
 
+MAX_AGE_GAP = 31
+
 
 def _browsing_put(id_user, request, search):
     json = request.json
@@ -43,11 +45,17 @@ def _search_gender_sexual_orientation(search, gender, sexual_orientation):
 
 
 def _search_age(search, age, age_gap):
+    if age == MAX_AGE_GAP:
+        return
+
     search["max_age"] = age + age_gap
     search["min_age"] = age - age_gap
 
 
 def _search_fame(search, fame, fame_gap):
+    if fame == -1:
+        return
+
     search["max_fame"] = fame + fame_gap
     search["min_fame"] = fame - fame_gap
 
@@ -68,13 +76,20 @@ def services_browsing(id_user, request):
     fame = user_db[10]
     sexual_orientation = user_db[5]
 
-    if request.method == "PUT":
-        if "age_gap" in request.json:
-            _search_age(search, age, request.json["age_gap"])
-        if "fame_gap" in request.json:
-            _search_fame(search, fame, request.json["fame_gap"])
-
     _search_gender_sexual_orientation(search, gender, sexual_orientation)
+
+    if request.method == "POST":
+        check_request = check_request_json(
+            request.headers.get("Content-Type"),
+            request.json,
+            ["ageGap", "fameGap", "distance"],
+        )
+
+        if check_request is not None:
+            return jsonify(check_request[0]), check_request[1]
+
+        _search_age(search, age, request.json["ageGap"])
+        _search_fame(search, fame, request.json["fameGap"])
 
     db_browsing_users = db_browsing_gender_sexualorientation(id_user, search)
 
