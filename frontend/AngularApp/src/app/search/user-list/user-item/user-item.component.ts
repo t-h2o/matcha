16bb1efea8ae2513f078bc515daabe008be48947
Robin Burri <robinburri@tutanota.com>
2024-12-12 09:52:44 +0100

    chat contact responsive

--- frontend/AngularApp/src/app/search/user-list/user-item/user-item.component.ts
+++ frontend/AngularApp/src/app/search/user-list/user-item/user-item.component.ts
@@ -1,29 +1,25 @@
 import { Component, inject, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { PossibleMatchesUserData } from '../../../shared/models/data-to-api/user';
-import { CardComponent } from '../../../UI/card/card.component';
 import {
   getFameRatingStars,
   getGender,
   getSexualPreference,
 } from '../../../shared/utils/displayUtils';
-// import { PotentialMatchService } from '../../../shared/services/potentialMatch.service';
 
 @Component({
   selector: 'app-user-item',
   standalone: true,
-  imports: [CardComponent],
+  imports: [],
   templateUrl: './user-item.component.html',
   styleUrl: './user-item.component.scss',
 })
 export class UserItemComponent {
   @Input({ required: true }) user!: PossibleMatchesUserData;
-  // private matchService = inject(PotentialMatchService);
   private router = inject(Router);
 
   onViewProfile(username: string) {
     this.router.navigate(['/search', username]);
-    // this.matchService.viewProfile(username);
   }
 
   get sexualPreference() {
