c4bf7ea4a62b1669fff86c3e198c1d5ad9fb93cb
|°_°| <grivel.theo@protonmail.com>
2024-12-12 16:42:21 +0100

    fix(import): name

--- backend/matcha/services/profile.py
+++ backend/matcha/services/profile.py
@@ -7,7 +7,7 @@ from flask_jwt_extended import (
 
 from matcha.app_utils import check_request_json
 
-from matcha.db.images import db_get_user_images, db_get_url_profile
+from matcha.db.pictures import db_get_user_images, db_get_url_profile
 
 from matcha.db.db import (
     db_get_interests,
