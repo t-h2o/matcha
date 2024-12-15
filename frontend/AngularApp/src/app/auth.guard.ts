0d1b48e485904de81ed9b47c683cf55c5ead0d4e
Robin Burri <robinburri@tutanota.com>
2024-12-15 15:31:03 +0100

    any user without profile_completed is redirected to /profile

--- frontend/AngularApp/src/app/auth.guard.ts
+++ frontend/AngularApp/src/app/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { AuthService } from './shared/services/auth.service';
+import { UserService } from './shared/services/user.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,14 +10,23 @@ export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router,
+    private userService: UserService,
   ) {}
 
-  canActivate(): boolean {
-    if (this.authService.tokenSignal()) {
-      return true;
-    } else {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
+    if (!this.authService.tokenSignal()) {
       this.router.navigate(['/login']);
       return false;
     }
+
+    if (
+      !this.userService.ownProfileData().profile_complete &&
+      route.routeConfig?.path !== 'profile'
+    ) {
+      this.router.navigate(['/profile']);
+      return false;
+    }
+
+    return true;
   }
 }
