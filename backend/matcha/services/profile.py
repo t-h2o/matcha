09ee645b615ad12622eedcf4448b10c86e041be7
|°_°| <grivel.theo@protonmail.com>
2024-12-17 20:17:06 +0100

    back(profile): return last connection date

--- backend/matcha/services/profile.py
+++ backend/matcha/services/profile.py
@@ -11,6 +11,8 @@ from matcha.db.pictures import db_get_user_images, db_get_url_profile
 
 from matcha.db.visit import db_put_visit, db_get_visit
 
+from matcha.db.last_connection import db_get_last_connection
+
 from matcha.db.db import (
     db_get_interests,
     db_set_user_profile_data,
@@ -121,6 +123,7 @@ def services_profile(id_user, request):
                 urlProfile=profile_url,
                 isLiked=is_liked,
                 connected=SocketManager.is_connected(user_db[0]),
+                lastConnection=db_get_last_connection(user_db[0]),
             ),
             200,
         )
