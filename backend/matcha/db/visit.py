e65cd8077cccdc23d852ac013366036a5ea19cef
|°_°| <grivel.theo@protonmail.com>
2024-12-17 18:30:39 +0100

    feat(profile): return visited by (#158)
    Backend
    
    - (profile) Return visitedBy
    
    Database
    
    - (visit) Insert visite by
    - (visit) Get visit by
    
    Features
    
    - (cliff) Add db sub-section
    
    Testing
    
    - (profile) Add visitedBy

---
+++ backend/matcha/db/visit.py
@@ -0,0 +1,53 @@
+from matcha.db.utils import (
+    db_query,
+    db_query_for,
+    db_fetchone,
+    db_fetchall,
+    fetchall_to_array,
+)
+
+
+def db_put_visit(id_visitor: int, id_visited: int):
+    query = """
+    INSERT INTO user_visits
+    (
+      visitor_id,
+      visited_id
+    )
+    VALUES
+    (
+      %s,
+      %s
+    );
+    """
+
+    return db_query(
+        query,
+        (
+            id_visitor,
+            id_visited,
+        ),
+    )
+
+
+def db_get_visit(id_visited: int):
+    query = """
+    SELECT username
+    FROM users
+    WHERE
+    id IN
+    (
+      SELECT visitor_id
+      FROM user_visits
+      WHERE
+      visited_id = %s
+    )
+    ;
+    """
+
+    return fetchall_to_array(
+        db_fetchall(
+            query,
+            (id_visited,),
+        )
+    )
