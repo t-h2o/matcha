1cda30fb7835300559643910ba19dfa236452653
|°_°| <grivel.theo@protonmail.com>
2024-12-24 19:47:12 +0100

    back(match): create a route

---
+++ backend/matcha/routes/match.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request
+
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity,
+)
+
+from matcha.services.match import services_match
+
+bp = Blueprint("match", __name__)
+
+
+@bp.route("/api/match")
+@jwt_required()
+def post_match():
+    id_user = get_jwt_identity()
+    return services_match(id_user)
