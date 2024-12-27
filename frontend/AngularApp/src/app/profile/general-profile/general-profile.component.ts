992cd2c5729ffed14abac708cd8a946076d5714c
Robin Burri <robinburri@tutanota.com>
2024-12-27 13:49:09 +0100

    short bio if too long

--- frontend/AngularApp/src/app/profile/general-profile/general-profile.component.ts
+++ frontend/AngularApp/src/app/profile/general-profile/general-profile.component.ts
@@ -44,6 +44,9 @@ export class GeneralProfileComponent {
   }
 
   get bio() {
+    if (this.userProfile().bio.length > 300) {
+      return getBio(this.userProfile().bio.substring(0, 300) + ' ...');
+    }
     return getBio(this.userProfile().bio);
   }
 
