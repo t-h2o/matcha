126760a57416c1f197bb4c1c80f940032f695f16
|°_°| <grivel.theo@protonmail.com>
2024-12-28 21:03:54 +0100

    back(utils): move send mail function

--- backend/matcha/routes/emails.py
+++ backend/matcha/routes/emails.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, current_app
 
-from smtplib import SMTP, SMTP_SSL
 
 from time import time
+
+from matcha.utils import send_mail
 
 
 bp = Blueprint("emails", __name__)
@@ -10,31 +11,5 @@ bp = Blueprint("emails", __name__)
 
 @bp.route("/mail")
 def mail():
-    start = time()
-
-    if current_app.config["MAIL_SMTP_METHOD"] == "ssl":
-        smtp_connection = SMTP_SSL(
-            host=current_app.config["MAIL_SMTP_HOST"],
-            port=current_app.config["MAIL_SMTP_PORT"],
-        )
-    else:
-        smtp_connection = SMTP(
-            host=current_app.config["MAIL_SMTP_HOST"],
-            port=current_app.config["MAIL_SMTP_PORT"],
-        )
-
-    resp_code, response = smtp_connection.login(
-        user=current_app.config["MAIL_USER"],
-        password=current_app.config["MAIL_PASSWORD"],
-    )
-
-    to_list = [current_app.config["MAIL_TEST"]]
-    message = "Subject: {}\n\n{}".format("Test Email", "body,\n\nbest,\n\nmatcha")
-
-    response = smtp_connection.sendmail(
-        from_addr=current_app.config["MAIL_USER"], to_addrs=to_list, msg=message
-    )
-
-    resp_code, response = smtp_connection.quit()
-
+    send_mail(current_app.config["MAIL_TEST"], "function body", "function")
     return "mail sent"
