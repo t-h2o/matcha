6dc7dc2b0809fa058248a6d22d0f9cbaa90a9b34
|°_°| <grivel.theo@protonmail.com>
2024-12-28 20:23:29 +0100

    back(mail): add smtp method

--- backend/matcha/routes/emails.py
+++ backend/matcha/routes/emails.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, current_app
 
-from smtplib import SMTP_SSL
+from smtplib import SMTP, SMTP_SSL
 
 from time import time
 
@@ -11,13 +11,17 @@ bp = Blueprint("emails", __name__)
 @bp.route("/mail")
 def mail():
     start = time()
-    smtp_ssl = SMTP_SSL(host=current_app.config["MAIL_SMTP_HOST"], port=current_app.config["MAIL_SMTP_PORT"])
 
-    print("Connection Object : {}".format(smtp_ssl))
+    if current_app.config["MAIL_SMTP_METHOD"] == "ssl":
+        smtp_connection = SMTP_SSL(host=current_app.config["MAIL_SMTP_HOST"], port=current_app.config["MAIL_SMTP_PORT"])
+    else:
+        smtp_connection = SMTP(host=current_app.config["MAIL_SMTP_HOST"], port=current_app.config["MAIL_SMTP_PORT"])
+
+    print("Connection Object : {}".format(smtp_connection))
     print("Total Time Taken  : {:,.2f} Seconds".format(time() - start))
 
     print("\nLogging In.....")
-    resp_code, response = smtp_ssl.login(
+    resp_code, response = smtp_connection.login(
         user=current_app.config["MAIL_USER"], password=current_app.config["MAIL_PASSWORD"]
     )
 
@@ -27,14 +31,14 @@ def mail():
     to_list = [current_app.config["MAIL_TEST"]]
     message = "Subject: {}\n\n{}".format("Test Email", "body,\n\nbest,\n\nmatcha")
 
-    response = smtp_ssl.sendmail(
+    response = smtp_connection.sendmail(
         from_addr=current_app.config["MAIL_USER"], to_addrs=to_list, msg=message
     )
 
     print("List of Failed Recipients : {}".format(response))
 
     print("\nLogging Out....")
-    resp_code, response = smtp_ssl.quit()
+    resp_code, response = smtp_connection.quit()
 
     print("Response Code : {}".format(resp_code))
     print("Response      : {}".format(response.decode()))
