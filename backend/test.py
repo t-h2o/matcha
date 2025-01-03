f405db562b9021a65fd95e56f12d4b08528b0f29
|°_°| <grivel.theo@protonmail.com>
2025-01-03 12:05:25 +0100

    test(reset): add test and move position

--- backend/test.py
+++ backend/test.py
@@ -532,6 +532,22 @@ def test_reset_password():
         "/api/reset-password",
         201,
         {"username": "user"},
+        {"error": "your email wasn't verified"},
+    )
+    check_post(
+        "/api/reset-password",
+        401,
+        {"username": "thisusernamedoesnotexist"},
+        {"error": "this username does not exist"},
+    )
+    check_login_token(
+        "/api/login",
+        {"username": "another", "password": "5678"},
+    )
+    check_post(
+        "/api/reset-password",
+        401,
+        {"username": "another"},
         {"success": "email with password reset link sent"},
     )
 
@@ -1663,7 +1679,6 @@ def main():
     test_interests()
     test_pictures()
     test_email()
-    test_reset_password()
     test_position()
     test_browsing()
     test_like_user()
@@ -1671,6 +1686,7 @@ def main():
     test_notification()
     test_fake()
     test_confirm()
+    test_reset_password()
     test_deleteme()
 
 
