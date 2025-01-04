bad9c3d89612adb5c776164218c5f82e06adfa0a
|°_°| <grivel.theo@protonmail.com>
2025-01-04 18:00:37 +0100

    back(route): create reset with jwt

--- backend/matcha/routes/auth.py
+++ backend/matcha/routes/auth.py
@@ -1,6 +1,10 @@
 from flask import Blueprint, request
 
-from matcha.services.auth import service_login_user, services_reset_password
+from matcha.services.auth import (
+    service_login_user,
+    services_reset_password,
+    services_reset_password_jwt,
+)
 
 bp = Blueprint("auth", __name__)
 
@@ -13,3 +17,8 @@ def login_user():
 @bp.route("/api/reset-password", methods=["POST"])
 def reset_password():
     return services_reset_password(request)
+
+
+@bp.route("/api/reset-password/<string:jwt>", methods=["POST"])
+def reset_password_jwt(jwt):
+    return services_reset_password_jwt(request, jwt)
