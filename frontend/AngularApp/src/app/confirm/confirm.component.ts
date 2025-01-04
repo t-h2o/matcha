f60b8c889850c0ff468f6638add4b2c896201fa7
Robin Burri <robinburri@tutanota.com>
2025-01-04 21:04:36 +0100

    confirm page created

---
+++ frontend/AngularApp/src/app/confirm/confirm.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm.component.html',
+  styleUrl: './confirm.component.scss',
+})
+export class ConfirmComponent {}
