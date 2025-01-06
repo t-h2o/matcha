cb350dc2b8c5c9ce1f7a5d0de68abb498c194060
Robin Burri <robinburri@tutanota.com>
2025-01-06 08:22:41 +0100

    create reset-page component

---
+++ frontend/AngularApp/src/app/reset-pwd-page/reset-pwd-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-pwd-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './reset-pwd-page.component.html',
+  styleUrl: './reset-pwd-page.component.scss'
+})
+export class ResetPwdPageComponent {
+
+}
