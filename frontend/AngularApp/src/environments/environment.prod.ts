baa74b44352f8301a7a2fe9d5d57b3d07d1f92a0
|°_°| <grivel.theo@protonmail.com>
2025-01-09 17:08:30 +0100

    port 4200

--- frontend/AngularApp/src/environments/environment.prod.ts
+++ frontend/AngularApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost/api',
-  websocketUrl: 'http://localhost',
+  apiUrl: 'http://localhost:4200/api',
+  websocketUrl: 'http://localhost:4200',
 };
